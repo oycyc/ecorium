f167ec55a5f5c4b11f7d1a6c61b9f17c797597eb
yangciou <me@yangciou.com>
2022-04-20 16:17:14 -0400

    two video testing

--- js/index.js
+++ js/index.js
@@ -35,39 +35,38 @@ function setSlide(prev, next) {
   document.getElementsByClassName('carousel-item')[slide].classList.add('active');
 };
 
+
+
+
+
+
 //
 // Scrolling Video Animation
 //
-
-const videoIntro = document.querySelector(".scrolling-vid-intro");
-const video = videoIntro.querySelector("video");
-const videoIntroText = videoIntro.querySelector("h1");
-// ending 
-const postVideo = document.getElementById("post-video");
-const postVideoText = postVideo.querySelector("h1");
-
-//SCROLLMAGIC
 const controller = new ScrollMagic.Controller();
 
-//Scenes
-let scene = new ScrollMagic.Scene({
-  duration: 8500,
-  triggerElement: videoIntro,
+// First Video - Protect Our Home
+const videoContainer1 = document.getElementById("scrolling-vid-1");
+const video1 = videoContainer1.querySelector("video");
+const video1Text = videoContainer1.querySelector("h1");
+
+let video1Scene = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: videoContainer1,
   triggerHook: 0
 })
-  .addIndicators()
-  .setPin(videoIntro)
+  .addIndicators() // for debugging
+  .setPin(videoContainer1)
   .addTo(controller);
 
-//Text Animation
-const textAnim = TweenMax.fromTo(videoIntroText, 3, { opacity: 1 }, { opacity: 0 });
-
-let scene2 = new ScrollMagic.Scene({
-  duration: 4000,
-  triggerElement: videoIntro,
+// Text Animation Scene
+const video1TextAnimation = TweenMax.fromTo(video1Text, 4, { opacity: 1 }, { opacity: 0 });
+let video1TextScene = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: videoContainer1,
   triggerHook: 0
 })
-  .setTween(textAnim)
+  .setTween(video1TextAnimation)
   .addTo(controller);
 
 //Video Animation
@@ -75,15 +74,55 @@ let accelamount = 0.1;
 let scrollpos = 0;
 let delay = 0;
 
-scene.on("progress", function (event) {
-  console.log("Scene progress changed to " + event.progress);
-  scrollpos = video.duration * event.progress;
+video1Scene.on("progress", function (event) {
+  // console.log("Scene progress changed to " + event.progress);
+  scrollpos = video1.duration * event.progress;
 });
-
 
 setInterval(() => {
   delay += (scrollpos - delay) * accelamount;
-  // console.log(scrollpos, delay);
+  video1.currentTime = delay;
+}, 33.3);
 
-  video.currentTime = delay;
+
+
+
+// Second Video - Don't Let This Happen 
+const videoContainer2 = document.getElementById("scrolling-vid-2");
+const video2 = videoContainer2.querySelector("video");
+const video2Text = videoContainer2.querySelector("h1");
+
+
+let video2Scene = new ScrollMagic.Scene({
+  duration: 2250,
+  triggerElement: videoContainer2,
+  triggerHook: 0
+})
+  .addIndicators() // for debugging
+  .setPin(videoContainer2)
+  .addTo(controller);
+
+// Text Animation Scene
+const video2TextAnimation = TweenMax.fromTo(video2Text, 3, { opacity: 1 }, { opacity: 0 });
+let video2TextScene = new ScrollMagic.Scene({
+  duration: 2250,
+  triggerElement: videoContainer2,
+  triggerHook: 0
+})
+  .setTween(video2TextAnimation)
+  .addTo(controller);
+
+//Video Animation
+let accelamount2 = 0.1;
+let scrollpos2 = 0;
+let delay2 = 0;
+
+video2Scene.on("progress", function (event) {
+  console.log("Scene progress changed to " + event.progress);
+  scrollpos2 = video2.duration * event.progress;
+});
+
+setInterval(() => {
+  delay2 += (scrollpos2 - delay2) * accelamount2;
+  video2.currentTime = delay2;
 }, 33.3);
