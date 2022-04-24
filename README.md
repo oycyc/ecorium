33c7d0ec7785b6cd7e365a2c9c66d51dce8c2c40
yangciou <me@yangciou.com>
2022-04-24 09:13:25 -0400

    foodie@

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ecorium
 
-## cici is my cute lil baby girl
+## todos
+
+### index.html
+- fix mobile navbar
+- would like another interactive/animation on the index.html
+
+### food.html
+- make horizontal images more round border radius or use svg image morphs
+- .slide-container span img (css) change height vh for mobile
---
+++ js/food.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+    var controller = new ScrollMagic.Controller();
+  
+    var horizontalSlide = new TimelineMax()
+    // animate panels
+    .to("#js-slideContainer", 1,   {x: "-20%"}) 
+    .to("#js-slideContainer", 1,   {x: "-40%"})
+    .to("#js-slideContainer", 1,   {x: "-60%"})
+    .to("#js-slideContainer", 1,   {x: "-80%"})
+  
+  
+    // create scene to pin and link animation
+    new ScrollMagic.Scene({
+      triggerElement: "#js-wrapper",
+      triggerHook: "onLeave",
+      duration: "400%"
+    })
+      .setPin("#js-wrapper")
+      .setTween(horizontalSlide)
+      .addTo(controller);
+  
+  
+  });
+    
+    
