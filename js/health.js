7372414eef5c810e77adb4e6929d84c1c6572430
yangciou <me@yangciou.com>
2022-05-05 17:58:28 -0400

    image in article

--- js/health.js
+++ js/health.js
@@ -12,3 +12,21 @@ for (let pollutedCard of allPollutedCards) {
 		pollutedCard.classList.add("active");
 	});
 };
+
+// Handle Image Click to view full size
+const imageMask = document.querySelector(".image-mask");
+const imageMaskInner = document.querySelector(".image-mask-inner");
+const imageItem = document.querySelector(".image-item");
+
+imageMask.addEventListener("click", () => {
+	imageMask.classList.add("mask-off");
+});
+
+imageItem.addEventListener("click", () => {
+	let styleValue = imageItem.getAttribute("style");
+	imageMaskInner.setAttribute(
+		"src",
+		styleValue.slice(styleValue.indexOf(": url(") + 7, -3)
+	);
+	imageMask.classList.remove("mask-off");
+});
