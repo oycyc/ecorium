39e089e3568fca2b5c645ed89441b1f390649107
yangciou <me@yangciou.com>
2022-05-06 19:45:05 -0400

    DELTED

--- js/food.js
+++ js/food.js
@@ -25,69 +25,71 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 // Food Carousel
 
-(function( $ ){
+// Food Carousel
 
-	// Count canvases
-	var slideCount = $( '.canvas' ).length,
-	paginationDot = '<div class="dot"></div>';
+// (function( $ ){
 
-	for ( var i = 1; i <= slideCount; i++ ) {
-		$( '.pagination-dot-container' ).append( paginationDot );
-	}
+// 	// Count canvases
+// 	var slideCount = $( '.canvas' ).length,
+// 	paginationDot = '<div class="dot"></div>';
 
-	// Add selected class to first dot
-	$( '.dot' ).first().addClass( 'selected' );
+// 	for ( var i = 1; i <= slideCount; i++ ) {
+// 		$( '.pagination-dot-container' ).append( paginationDot );
+// 	}
 
-	// Build Carousel
-	var dot = 0,
-	length = $( '.dot' ).length - 1,
-	winWidth = $( window ).width(),
-	messageBoxWidth = $( '.message-box' ).width(),
+// 	// Add selected class to first dot
+// 	$( '.dot' ).first().addClass( 'selected' );
 
-	slideFunction = function() {
-		var currentDot = $( '.dot' ).eq( dot ),
-		currentSlide = $( '.canvas' ).eq( dot ),
-		currentCard = $( '.card' ).eq( dot );
+// 	// Build Carousel
+// 	var dot = 0,
+// 	length = $( '.dot' ).length - 1,
+// 	winWidth = $( window ).width(),
+// 	messageBoxWidth = $( '.message-box' ).width(),
 
-		currentDot.addClass( 'selected' );
-		currentCard.addClass( 'activeCard' );
-		$( '.dot' ).not( currentDot ).removeClass( 'selected' );
-		$( '.card' ).not( currentCard ).removeClass( 'activeCard' ).removeClass( 'prevCard' );
-		$( '.activeCard' ).prev().addClass( 'prevCard' );
+// 	slideFunction = function() {
+// 		var currentDot = $( '.dot' ).eq( dot ),
+// 		currentSlide = $( '.canvas' ).eq( dot ),
+// 		currentCard = $( '.card' ).eq( dot );
 
-		$( '.canvas-container' ).animate({ 'left': - dot * winWidth }, 500);
-    console.log(dot);
-    console.log(winWidth);
-		$( '.card-container' ).css( 'left', - dot * messageBoxWidth );
-	}
+// 		currentDot.addClass( 'selected' );
+// 		currentCard.addClass( 'activeCard' );
+// 		$( '.dot' ).not( currentDot ).removeClass( 'selected' );
+// 		$( '.card' ).not( currentCard ).removeClass( 'activeCard' ).removeClass( 'prevCard' );
+// 		$( '.activeCard' ).prev().addClass( 'prevCard' );
 
-	$( '.pagination-forward-container' ).on( 'click', function() {
-		dot = dot < length ? dot += 1 : 0;
-		slideFunction();
-	});
+// 		$( '.canvas-container' ).animate({ 'left': - dot * winWidth }, 500);
+//     console.log(dot);
+//     console.log(winWidth);
+// 		$( '.card-container' ).css( 'left', - dot * messageBoxWidth );
+// 	}
 
-	$( '.dot' ).on( 'click', function() {
-		dot = $( this ).index( '.dot' );
-		slideFunction();
-		$( '.card' ).not( currentCard ).removeClass( 'activeCard' ).removeClass( 'prevCard' );
-		$( '.activeCard' ).prev().addClass( 'prevCard' );
-	});
+// 	$( '.pagination-forward-container' ).on( 'click', function() {
+// 		dot = dot < length ? dot += 1 : 0;
+// 		slideFunction();
+// 	});
 
-	var carouselSetup = function() {
-		winWidth = $( window ).width();
-		var canvasCount = $( '.canvas' ).length,
-		cardCount = $( '.card' ).length;
+// 	$( '.dot' ).on( 'click', function() {
+// 		dot = $( this ).index( '.dot' );
+// 		slideFunction();
+// 		$( '.card' ).not( currentCard ).removeClass( 'activeCard' ).removeClass( 'prevCard' );
+// 		$( '.activeCard' ).prev().addClass( 'prevCard' );
+// 	});
 
-		$( '.canvas' ).css( 'width', 'calc(100%/' + canvasCount + ' )' );
-		$( '.canvas-container' ).css( 'width', winWidth * canvasCount );
-		$( '.card-container' ).css( 'width', messageBoxWidth * cardCount );
-	}
+// 	var carouselSetup = function() {
+// 		winWidth = $( window ).width();
+// 		var canvasCount = $( '.canvas' ).length,
+// 		cardCount = $( '.card' ).length;
 
-	$( window ).on( 'load resize', function() {
-		carouselSetup();
-	});
+// 		$( '.canvas' ).css( 'width', 'calc(100%/' + canvasCount + ' )' );
+// 		$( '.canvas-container' ).css( 'width', winWidth * canvasCount );
+// 		$( '.card-container' ).css( 'width', messageBoxWidth * cardCount );
+// 	}
 
-})(jQuery);
+// 	$( window ).on( 'load resize', function() {
+// 		carouselSetup();
+// 	});
+
+// })(jQuery);
 
 
 // function getWidth(el) {
