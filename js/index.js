e8128cdd61e5596d223e41e8b688c7fadd3dad07
yangciou <me@yangciou.com>
2022-05-06 20:51:16 -0400

    temp delete

--- js/index.js
+++ js/index.js
@@ -3,37 +3,37 @@ Did You Know Carousel
 */
 
 // make the first carousel is always active
-document.getElementsByClassName('carousel-item')[0].classList.add('active');
-let total =  document.getElementsByClassName('carousel-item').length;
-let current = 0;
+// document.getElementsByClassName('carousel-item')[0].classList.add('active');
+// let total =  document.getElementsByClassName('carousel-item').length;
+// let current = 0;
 
-document.getElementById('moveRight').addEventListener('click', function() {
-  let next = current;
-  current = current + 1;
-  setSlide(next, current);
-});
+// document.getElementById('moveRight').addEventListener('click', function() {
+//   let next = current;
+//   current = current + 1;
+//   setSlide(next, current);
+// });
 
-document.getElementById('moveLeft').addEventListener('click', function() {
-  let prev = current;
-  current = current - 1;
-  setSlide(prev, current);
-});
+// document.getElementById('moveLeft').addEventListener('click', function() {
+//   let prev = current;
+//   current = current - 1;
+//   setSlide(prev, current);
+// });
 
-function setSlide(prev, next) {
-  let slide = current;
-  if (next > total - 1) {
-    slide = 0;
-    current = 0;
-  }
+// function setSlide(prev, next) {
+//   let slide = current;
+//   if (next > total - 1) {
+//     slide = 0;
+//     current = 0;
+//   }
 
-  if (next < 0) {
-    slide = total - 1;
-    current = total - 1;
-  }
+//   if (next < 0) {
+//     slide = total - 1;
+//     current = total - 1;
+//   }
 
-  document.getElementsByClassName('carousel-item')[prev].classList.remove('active');
-  document.getElementsByClassName('carousel-item')[slide].classList.add('active');
-};
+//   document.getElementsByClassName('carousel-item')[prev].classList.remove('active');
+//   document.getElementsByClassName('carousel-item')[slide].classList.add('active');
+// };
 
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
