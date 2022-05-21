58976cc936e40f1e7f3be825903a2dbf6fd04b47
yangciou <me@yangciou.com>
2022-05-21 15:43:32 -0400

    food carousel

--- js/food.js
+++ js/food.js
@@ -18,3 +18,49 @@ document.addEventListener("DOMContentLoaded", function (event) {
     .setTween(horizontalSlide)
     .addTo(controller);
 });
+
+
+// food carousel
+const items = document.querySelectorAll('.slider-item');
+const itemCount = items.length;
+const nextItem = document.querySelector('.next');
+const previousItem = document.querySelector('.previous');
+let count = 0;
+
+function showNextItem() {
+  items[count].classList.remove('active');
+
+  if(count < itemCount - 1) {
+    count++;
+  } else {
+    count = 0;
+  }
+
+  items[count].classList.add('active');
+}
+
+function showPreviousItem() {
+  items[count].classList.remove('active');
+
+  if(count > 0) {
+    count--;
+  } else {
+    count = itemCount - 1;
+  }
+
+  items[count].classList.add('active');
+}
+
+function keyPress(e) {
+  e = e || window.event;
+  
+  if (e.keyCode == '37') {
+    showPreviousItem();
+  } else if (e.keyCode == '39') {
+    showNextItem();
+  }
+}
+
+nextItem.addEventListener('click', showNextItem);
+previousItem.addEventListener('click', showPreviousItem);
+document.addEventListener('keydown', keyPress);
